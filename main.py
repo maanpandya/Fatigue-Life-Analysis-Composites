10b9f516bf4a07cd14eaeacb89cd704131208e55
cawass <carloscarrascorequejo@gmail.com>
2024-02-20 10:47:33 +0100

    updated pandas code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
-dataframe = pd.read_csv("Optidat UPWIND 28_3_2017.csv")
+dataframe = pd.read_csv(r".\\Optidat UPWIND 28_3_2017.csv")
+print(dataframe.head(10))
 print(dataframe)
+##hola
