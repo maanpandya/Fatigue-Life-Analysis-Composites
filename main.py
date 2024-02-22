4b29a795b4ba60ca26acf73492d3330a0f41a664
maanpandya <maanbhavinpandya@gmail.com>
2024-02-22 15:23:48 +0100

    #

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
-dataframe = pd.read_csv(r".\\Optidat UPWIND 28_3_2017.csv")
+dataframe = pd.read_csv(r"Databases\Optidat UPWIND 28_3_2017.csv")
 print(dataframe.head(10))
 print(dataframe)
 ##hola
 
+##there is also a session chat u can check out
+##where?
+##nvm
+##found it
+#hello
