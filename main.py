958b158f4036cbe970fd9d20c2247ac72129733d
maanpandya <maanbhavinpandya@gmail.com>
2024-03-12 10:26:07 +0100

    Implemented time tagged model paths, trying to find the optimal network structure

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
         self.dummy_param = nn.Parameter(torch.empty(0))
-        self.layer1 = nn.Linear(10, 10)
-        self.layer2 = nn.Linear(10, 10)
-        self.layer3 = nn.Linear(10, 10)
-        self.layer4 = nn.Linear(10, 10)
-        self.layer5 = nn.Linear(10, 10)
-        self.layer6 = nn.Linear(10, 1)
+        self.layer1 = nn.Linear(10, 12)
+        self.layer2 = nn.Linear(12, 12)
+        self.layer3 = nn.Linear(12, 12)
+        self.layer4 = nn.Linear(12, 12)
+        self.layer5 = nn.Linear(12, 12)
+        self.layer6 = nn.Linear(12, 1)
 
     def forward(self, x):
         device = self.dummy_param.device
@@ -33,16 +33,18 @@ model.to('cuda')
 print(model.dummy_param.device)
 
 #Optimizer
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
 
 #Loss functions
-criterion = nn.L1Loss()
+criterion = nn.MSELoss()
 
 # Load the data from "NNTrainingData.csv"
 base = pd.read_csv("Data processing/processed/traindata070324170302.csv")
 base = base.set_index('nr')
 data = dp.col_filter(base, ['Ncycles'], 'exclude')
 target = dp.col_filter(base, ['Ncycles'], 'include')
+print(data)
+print(target)
 
 # Extract the input data from the first 10 columns
 X = torch.tensor(data.iloc[:, :10].values)
@@ -50,11 +52,11 @@ X = X.cuda()
 print(X.device)
 
 # Extract the output data from the last column
-y = torch.tensor(data.iloc[:, -1].values).view(-1, 1)
+y = torch.tensor(target.iloc[:, -1].values).view(-1, 1)
 y = y.cuda()
 print(y.device)
 
-#Loss
+#L1
 losses = []
 
 #Forward pass
@@ -81,11 +83,11 @@ for epoch in range(50000):
 #Plot the loss
 plt.plot(losses)
 plt.xlabel('Epoch')
-plt.ylabel('Loss')
+plt.ylabel('L1')
 plt.show()
 
 #Test the model
-test_data = pd.read_csv("Data processing/processed/testdata070324170302.csv")
+'''test_data = pd.read_csv("Data processing/processed/testdata070324170302.csv")
 X_test = torch.tensor(test_data.iloc[:, :10].values)
 X_test = X_test.cuda()
 y_test = torch.tensor(test_data.iloc[:, -1].values).view(-1, 1)
@@ -93,7 +95,8 @@ y_test = y_test.cuda()
 y_test_pred = model(X_test)
 loss = criterion(y_test_pred, y_test)
 print(loss.item())
-print(y_test_pred-y_test)
+print(y_test_pred-y_test)'''
 
 #Save the model
-torch.save(model.state_dict(), 'model.pth')
+path = 'NNModelArchive/model'+ dp.timetag() +'.pth'
+torch.save(model.state_dict(), path)
