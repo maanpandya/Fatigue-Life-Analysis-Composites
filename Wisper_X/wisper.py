cf50a8c041e8c8552e0966b81e605e56a76e71ad
cawass <carloscarrascorequejo@gmail.com>
2024-03-21 14:23:03 +0100

    Update wisper.py

--- Wisper_X/wisper.py
+++ Wisper_X/wisper.py
@@ -7,7 +7,13 @@ with open('Wisper_X/WISPERX', 'r') as file:
 
 array = np.array([float(x) for x in file_contents[1:]])
 
+# Normalize the array to the maximum value
+normalized_array = array / np.max(array)
+
+# Count cycles
+counted_cycles = rainflow.count_cycles(normalized_array)
+
 
 # Print the counted cycles
 print(counted_cycles)
-print(array)
+print(normalized_array)
