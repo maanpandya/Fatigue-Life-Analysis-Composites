0c16d77c676ece054ec9d2f3af4a8499134e25e2
cawass <carloscarrascorequejo@gmail.com>
2024-03-26 12:01:54 +0100

    Stupid stupid stupid

--- Wisper/Wisper.py
+++ Wisper/Wisper.py
@@ -9,7 +9,7 @@ import torch
 import pandas as pd
 import DataProcessing.DPfunctions as dp
 
-path = 'NeuralNetworkCode/NNModelArchive/rev2/10x30pinlossbetter'
+path = 'NeuralNetworkCode/NNModelArchive/rev2/0449mse4mrenoisetrained'
 model, scaler = f.import_model(path)
 
 # Read the file
@@ -30,16 +30,16 @@ print(counted_cycles)
 
 stresses = []
 cycles = []
+Stress_test = []
+N_test = []
 for j in range(340):
     Accumulated_stress = 0
     for i in range (len(counted_cycles)):
         N_Cycle =counted_cycles[i][1] # fatigue life at reference stress 
-        Stress = counted_cycles[i][0] * (j)# stress
-        print(Stress)
-        B = 1.69111*10**22
-        data2 = pd.read_csv('NeuralNetworkCode/DataProcessing/processed/testdata2.csv')
+        Stress = counted_cycles[i][0] * (340-j)# stress
+        data2 = pd.read_csv('NeuralNetworkCode/DataProcessing/processed/traindata2.csv')
         x = pd.DataFrame(np.nan,index=[0],columns=data2.columns)
-        x['smax'] = Stress#Max Stress
+        x['smax'] = Stress #Max Stress
         x['Fibre Volume Fraction'] =53.26 #Fibre Volume Fraction
         x['Cut angle '] = 0 #Cut angle
         x['taverage'] = 6.45 #Average thickness
@@ -56,20 +56,33 @@ for j in range(340):
         #Normalize the data
         for i in x.columns:
             x[i] = (x[i] - scaler[i]['mean']) / scaler[i]['std']
+
         
         #Predict the number of cycles
-        x = torch.tensor(x.values)
+        xtest = torch.tensor(x.values)
 
-        x=x.cuda()
+        xtest=xtest.cuda()
+        print(x)
         model.eval()
-        N_AI = model(x)
-        N_AI = N_AI.cpu().detach().numpy()
+        print(model(xtest))
+        N_AI = model(xtest) 
+        N_AI = N_AI.cpu().detach().item()
         N_AI = np.power(10, N_AI)
-        print(N_AI)
+    
+        N_test.append(N_AI)
+        Stress_test.append(Stress)
+        
         Accumulated_stress += N_Cycle/N_AI
+    fig = plt.figure()
+    print(N_test)
+    print(Stress_test)
+    plt.plot(N_test, Stress_test, color='tab:blue')  # Use plt.plot() instead of fig.plot()
+    plt.show()
+    print(1/Accumulated_stress)
     cycles.append(1/Accumulated_stress)
+    print(j)
     stresses.append(j)
-plt.plot(np.log(cycles), stresses)  # Changed the order of the axes
+plt.plot(cycles, stresses)  # Changed the order of the axes
 plt.xlabel('N_AI')  # Updated the x-axis label
 plt.ylabel('Stress')  # Updated the y-axis label
 plt.title('N_AI vs Stress')
