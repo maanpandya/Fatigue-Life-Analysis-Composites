47e05cfc024173b68a2bbb3ab7b4e1d567909ee4
Yan-Tavares <y.cavalcante.ae@gmail.com>
2024-03-26 09:32:00 +0100

    Yes, I am sure I want to commit these changes.

---
+++ CurveModelling/Data_processing.py
@@ -0,0 +1,29 @@
+def separateDataFrame(dataFrame, separationList = ["R-value1"]):
+
+    # separationList - Parameters to separate by, r value by default
+    # data2.csv has nr,Fibre Volume Fraction,Cut angle ,taverage,waverage,area,Lnominal,R-value1,Ffatigue,smax,Ncycles,f,E,Temp.
+
+    # set up dictionary with keys ("parameter" + " " + "parameterValue" ) and the list of values for that parameter and marameterValue
+    parameterDictionary = dict()
+
+    for parameter in separationList:  # for each parameter that needs to be separated
+
+        # Set up a list of the possible parameter values
+        parameterValues = [dataFrame[parameter][0]]  # get one starting parameter value
+        for index, row in enumerate(dataFrame[parameter]):
+            counter = 0
+            for parameterValue in parameterValues:
+                if row != parameterValue:
+                    counter += 1
+                    if counter == len(parameterValues):  # if you find that there are more parameters that don't
+                        # match than you know of
+                        parameterValues.append(row)
+
+        for parameterValue in parameterValues:  # for each parameter value
+            key = str(parameter) + " " + str(parameterValue)
+            parameterDictionary[key] = pd.DataFrame()
+            for index, row in enumerate(dataFrame[parameter]):
+                if row == parameterValue:
+                    parameterDictionary[key] = parameterDictionary[key]._append(dataFrame.iloc[index])
+
+        return parameterDictionary
