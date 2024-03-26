1394a7b7ba7eac92b9db20e31351585292589a7f
cawass <carloscarrascorequejo@gmail.com>
2024-03-26 09:03:08 +0100

    Integration neural network and wisper

--- Wisper/Wisper.py
+++ Wisper/Wisper.py
@@ -1,10 +1,14 @@
+import sys
+sys.path.append('NeuralNetworkCode/DataProcessing')
+
 import numpy as np
 import rainflow 
 import matplotlib.pyplot as plt
 import function as f
 import torch
 import pandas as pd
-import DataProcessing.DPfunctions as dp
+import DataProcessing.DPfunctions as dp 
+
 path = 'NeuralNetworkCode/NNModelArchive/rev2/10x30pinloss'
 model, scaler = f.import_model(path)
 
@@ -34,18 +38,18 @@ for i in range (len(counted_cycles)):
     #N_AI = (Stress/(367.8))**(-1/0.078)
     data2 = pd.read_csv('NeuralNetworkCode/DataProcessing/processed/testdata2.csv')
     x = pd.DataFrame(columns=data2.columns)
-    x['smax'] = #Max Stress
-    x['Fibre Volume Fraction'] = #Fibre Volume Fraction
-    x['Cut angle'] = #Cut angle
-    x['taverage'] = #Average thickness
-    x['waverage'] = #Average width
-    x['area'] = #Area
-    x['Lnominal'] = #Nominal length of sample
-    x['R-value1'] = #R-value
-    x['Ffatigue'] = #Fatigue force
-    x['f'] = #Frequency
-    x['E'] = #Young's modulus
-    x['Temp'] = #Temperature
+    x['smax'] = Stress#Max Stress
+    x['Fibre Volume Fraction'] =53.26 #Fibre Volume Fraction
+    x['Cut angle'] = 0#Cut angle
+    x['taverage'] = 6.45#Average thickness
+    x['waverage'] = 25.25#Average width
+    x['area'] = 162.86#Area
+    x['Lnominal'] = 150#Nominal length of sample
+    x['R-value1'] = -1#R-value
+    x['Ffatigue'] = 53.75#Fatigue force
+    x['f'] = 363#Frequency
+    x['E'] = 30#Young's modulus
+    x['Temp'] = 28 #Temperature
 
     #Normalize the data
     for i in x.columns:
