1f02f4fdfa11ad40aa6dfe97a97d445682961a70
maanpandya <maanbhavinpandya@gmail.com>
2024-03-26 12:01:14 +0100

    carlos skill issue fix

--- Wisper/Wisper.py
+++ Wisper/Wisper.py
@@ -30,6 +30,26 @@ print(counted_cycles)
 
 stresses = []
 cycles = []
+data2 = pd.read_csv('NeuralNetworkCode/DataProcessing/processed/testdata2.csv')
+x = pd.DataFrame(np.nan,index=[0],columns=data2.columns)
+x['Fibre Volume Fraction'] =53.26 #Fibre Volume Fraction
+x['Cut angle '] = 0 #Cut angle
+x['taverage'] = 6.45 #Average thickness
+x['waverage'] = 25.25 #Average width
+x['area'] = 162.86 #Area
+x['Lnominal'] = 150 #Nominal length of sample
+x['R-value1'] = -1 #R-value
+x['Ffatigue'] = 53.75 #Fatigue force
+x['f'] = 3.63 #Frequency
+x['E'] = 30 #Young's modulus
+x['Temp.'] = 28 #Temperature
+x.drop(columns=['nr','Ncycles'],inplace=True)
+
+#Normalize the data except for stress
+for i in x.columns:
+    if i != 'smax':
+        x[i] = (x[i] - scaler[i]['mean']) / scaler[i]['std']
+
 for j in range(340):
     Accumulated_stress = 0
     for i in range (len(counted_cycles)):
@@ -37,36 +57,23 @@ for j in range(340):
         Stress = counted_cycles[i][0] * (j)# stress
         print(Stress)
         B = 1.69111*10**22
-        data2 = pd.read_csv('NeuralNetworkCode/DataProcessing/processed/testdata2.csv')
-        x = pd.DataFrame(np.nan,index=[0],columns=data2.columns)
-        x['smax'] = Stress#Max Stress
-        x['Fibre Volume Fraction'] =53.26 #Fibre Volume Fraction
-        x['Cut angle '] = 0 #Cut angle
-        x['taverage'] = 6.45 #Average thickness
-        x['waverage'] = 25.25 #Average width
-        x['area'] = 162.86 #Area
-        x['Lnominal'] = 150 #Nominal length of sample
-        x['R-value1'] = -1 #R-value
-        x['Ffatigue'] = 53.75 #Fatigue force
-        x['f'] = 363 #Frequency
-        x['E'] = 30 #Young's modulus
-        x['Temp.'] = 28 #Temperature
-        x.drop(columns=['nr','Ncycles'],inplace=True)
+        print(x)
+        x['smax'] = Stress #Max Stress
 
-        #Normalize the data
-        for i in x.columns:
-            x[i] = (x[i] - scaler[i]['mean']) / scaler[i]['std']
-        
+        #Normalize the data for smax
+        x['smax'] = (x['smax'] - scaler['smax']['mean']) / scaler['smax']['std']
+
         #Predict the number of cycles
-        x = torch.tensor(x.values)
+        xtest = torch.tensor(x.values)
 
-        x=x.cuda()
-        model.eval()
-        N_AI = model(x)
+        xtest=xtest.cuda()
+        N_AI = model(xtest)
         N_AI = N_AI.cpu().detach().numpy()
+        print(N_AI)
         N_AI = np.power(10, N_AI)
         print(N_AI)
         Accumulated_stress += N_Cycle/N_AI
+    
     cycles.append(1/Accumulated_stress)
     stresses.append(j)
 plt.plot(np.log(cycles), stresses)  # Changed the order of the axes
