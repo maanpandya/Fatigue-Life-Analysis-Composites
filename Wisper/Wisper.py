c8eb8ee1f1625ad987a7c2a823e495334001b36b
cawass <carloscarrascorequejo@gmail.com>
2024-03-26 10:14:51 +0100

    Wisper fixes

--- Wisper/Wisper.py
+++ Wisper/Wisper.py
@@ -27,57 +27,45 @@ counted_cycles = rainflow.count_cycles(normalized_array)
 Max_Force = 330 # Mpa
 
 print(counted_cycles)
-Accumulated_stress = 0
-for i in range (len(counted_cycles)):
-    N_Cycle =counted_cycles[i][1] # fatigue life at reference stress 
-    Stress = counted_cycles[i][0] * Max_Force# stress
-    B = 1.69111*10**22
-    #N_AI = -np.log(Stress/178.08)*10**(-7)
-
-    #Number of cycles predicted by AI model
-    #N_AI = (Stress/(367.8))**(-1/0.078)
-    data2 = pd.read_csv('NeuralNetworkCode/DataProcessing/processed/testdata2.csv')
-    x = pd.DataFrame(np.nan,index=[0],columns=data2.columns)
-    x['smax'] = Stress#Max Stress
-    x['Fibre Volume Fraction'] =53.26 #Fibre Volume Fraction
-    x['Cut angle '] = 0 #Cut angle
-    x['taverage'] = 6.45 #Average thickness
-    x['waverage'] = 25.25 #Average width
-    x['area'] = 162.86 #Area
-    x['Lnominal'] = 150 #Nominal length of sample
-    x['R-value1'] = -1 #R-value
-    x['Ffatigue'] = 53.75 #Fatigue force
-    x['f'] = 363 #Frequency
-    x['E'] = 30 #Young's modulus
-    x['Temp.'] = 28 #Temperature
-    x.drop(columns=['nr','Ncycles'],inplace=True)
-
-    #Normalize the data
-    for i in x.columns:
-        x[i] = (x[i] - scaler[i]['mean']) / scaler[i]['std']
-    
-    #Predict the number of cycles
-    x = torch.tensor(x.values)
-    x=x.cuda()
-    model.eval()
-    N_AI = model(x)
-    N_AI = N_AI.cpu().detach().numpy()
-
-    Accumulated_stress += N_Cycle/N_AI
 
 stresses = []
 cycles = []
 for j in range(340):
-    Max_Force = j
     Accumulated_stress = 0
     for i in range (len(counted_cycles)):
         N_Cycle =counted_cycles[i][1] # fatigue life at reference stress 
-        Stress = counted_cycles[i][0] * Max_Force# stress
-        N_AI = (Stress/(367.8))**(-1/0.078)
+        Stress = counted_cycles[i][0] * (340-j)# stress
+        B = 1.69111*10**22
+        data2 = pd.read_csv('NeuralNetworkCode/DataProcessing/processed/testdata2.csv')
+        x = pd.DataFrame(np.nan,index=[0],columns=data2.columns)
+        x['smax'] = Stress#Max Stress
+        x['Fibre Volume Fraction'] =53.26 #Fibre Volume Fraction
+        x['Cut angle '] = 0 #Cut angle
+        x['taverage'] = 6.45 #Average thickness
+        x['waverage'] = 25.25 #Average width
+        x['area'] = 162.86 #Area
+        x['Lnominal'] = 150 #Nominal length of sample
+        x['R-value1'] = -1 #R-value
+        x['Ffatigue'] = 53.75 #Fatigue force
+        x['f'] = 363 #Frequency
+        x['E'] = 30 #Young's modulus
+        x['Temp.'] = 28 #Temperature
+        x.drop(columns=['nr','Ncycles'],inplace=True)
+        print(x)
+
+        #Normalize the data
+        for i in x.columns:
+            x[i] = (x[i] - scaler[i]['mean']) / scaler[i]['std']
+        
+        #Predict the number of cycles
+        x = torch.tensor(x.values)
+
+        x=x.cuda()
+        model.eval()
+        N_AI = model(x)
+        N_AI = N_AI.cpu().detach().numpy()
+        N_AI = np.power(10, N_AI)
         Accumulated_stress += N_Cycle/N_AI
-    print()    
-    print(j)
-    print
     cycles.append(1/Accumulated_stress)
     stresses.append(j)
 plt.plot(np.log(cycles), stresses)  # Changed the order of the axes
