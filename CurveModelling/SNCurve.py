ab03b9086be4d759814dee97d534d34625b0cf7c
janek <j.zuchowski@student.tudelft.nl>
2024-03-26 09:24:20 +0100

    Added folder for S-N curves and CL diagrams

--- CurveModelling/SNCurve.py
+++ CurveModelling/SNCurve.py
@@ -2,30 +2,37 @@ import pandas as pd
 import numpy as np
 
 # Read csv
-dfmain = pd.read_csv("C:\Jan\Studia\Works\Year 2\Test, Analysis & Simulation\Code\Fatigue-Life-Analysis-Composites\CurveModelling\Data\data2.csv")
+dfMain = pd.read_csv("Data/data2.csv")
 
-# Parameters to seperate by
-# data2.csv has nr,Fibre Volume Fraction,Cut angle ,taverage,waverage,area,Lnominal,R-value1,Ffatigue,smax,Ncycles,f,E,Temp.
-seperation = ["R-value1"]
+def separateDataFrame(dataFrame, separationList = ["R-value1"]):
 
-parameterDictionary = dict()
+    # separationList - Parameters to separate by, r value by default
+    # data2.csv has nr,Fibre Volume Fraction,Cut angle ,taverage,waverage,area,Lnominal,R-value1,Ffatigue,smax,Ncycles,f,E,Temp.
 
-for parameter in seperation:
+    # set up dictionary with keys ("parameter" + " " + "parameterValue" ) and the list of values for that parameter and marameterValue
+    parameterDictionary = dict()
 
-    # Set up a list of the possible parameter values
-    parameterValues = [dfmain[parameter][0]]
-    for index, row in enumerate(dfmain[parameter]):
-        counter = 0
-        for parameterValue in parameterValues:
-            if row != parameterValue:
-                counter += 1
-                if counter == len(parameterValues):
-                    parameterValues.append(row)             
+    for parameter in separationList:  # for each parameter that needs to be separated
 
-    for parameterValue in parameterValues:
-        key = str(parameter) + " " + str(parameterValue)
-        parameterDictionary[key] = pd.DataFrame()
-        for index, row in enumerate(dfmain[parameter]):
-            if row == parameterValue:
-                parameterDictionary[key] = parameterDictionary[key]._append(dfmain.iloc[index])
+        # Set up a list of the possible parameter values
+        parameterValues = [dataFrame[parameter][0]]  # get one starting parameter value
+        for index, row in enumerate(dataFrame[parameter]):
+            counter = 0
+            for parameterValue in parameterValues:
+                if row != parameterValue:
+                    counter += 1
+                    if counter == len(parameterValues):  # if you find that there are more parameters that don't
+                        # match than you know of
+                        parameterValues.append(row)
 
+        for parameterValue in parameterValues:  # for each parameter value
+            key = str(parameter) + " " + str(parameterValue)
+            parameterDictionary[key] = pd.DataFrame()
+            for index, row in enumerate(dataFrame[parameter]):
+                if row == parameterValue:
+                    parameterDictionary[key] = parameterDictionary[key]._append(dataFrame.iloc[index])
+
+        return parameterDictionary
+
+dictionary = separateDataFrame(dfMain)
+print(dictionary)
