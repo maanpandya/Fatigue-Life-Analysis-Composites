e5ad09179dfa13915297b3d824de5e53e62dc0e5
janek <j.zuchowski@student.tudelft.nl>
2024-04-30 12:30:22 +0200

    modified:   CurveModelling/CLD_surface.py 	modified:   CurveModelling/__pycache__/CLD_definition.cpython-312.pyc

--- CurveModelling/CLD_surface.py
+++ CurveModelling/CLD_surface.py
@@ -4,24 +4,30 @@ import pandas as pd
 
 import CLD_definition
 
+# x axis - mean stress
+# y axis - stress amplitude
+# value - number of cycles
+
+
 #Create a dataframe out of the csv file
 dataframe = pd.read_csv("CurveModelling/Data/altdata.csv")
+UTS = 820
+UCS = -490
 CLD_definition.add_amplitudecol(dataframe)
 
 R_values, R_slopes_coeff, SN_models, ax = CLD_definition.CLD_definition(dataframe, plot=False)
 
-values = [x/10. for x in range(1,10)]
-coords = np.zeros((len(values),2))
+lives = [x/10. for x in range(1,10)]
 
 amp_plot_lists = []
 mean_plot_lists = []
 
-for life in values:
+for life in lives:
     amp_list = []
     mean_list = []
 
-    mean_list.append(-490)
-    amp_list.append(0)
+    # mean_list.append(UCS)
+    # amp_list.append(0)
 
     for i in range(len(SN_models)):
         #----Add STC and STT points
@@ -31,10 +37,16 @@ for life in values:
         amp_list.append(amp)
         mean_list.append(mean)
     
-    mean_list.append(820)
-    amp_list.append(0)
+    # mean_list.append(UTS)
+    # amp_list.append(0)
 
     amp_plot_lists.append(amp_list)
     mean_plot_lists.append(mean_list)
 
-coords = mean_plot_lists
+coords = np.array([])
+values = np.array([])
+# coords.append([UCS, 0], [UTS,0])
+# values.append([0],[0])
+print(coords, values)
+
+coords = np.array(mean_plot_lists,amp_plot_lists)
