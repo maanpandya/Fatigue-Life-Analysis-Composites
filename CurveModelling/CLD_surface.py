2e4a9fa1317fbb5ecc99e45e887cfa7e95296be0
janek <j.zuchowski@student.tudelft.nl>
2024-04-30 19:34:51 +0200

    modified:   CurveModelling/CLD_surface.py

--- CurveModelling/CLD_surface.py
+++ CurveModelling/CLD_surface.py
@@ -1,6 +1,9 @@
 import scipy as sp
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+from matplotlib import cm
 
 import CLD_definition
 
@@ -15,38 +18,37 @@ UTS = 820
 UCS = -490
 CLD_definition.add_amplitudecol(dataframe)
 
-R_values, R_slopes_coeff, SN_models, ax = CLD_definition.CLD_definition(dataframe, plot=False)
+R_values, R_slopes_coeff, SN_models, ax = CLD_definition.CLD_definition(dataframe, plot=True)
 
 lives = [x/10. for x in range(1,10)]
+coords = []
+values = []
 
-amp_plot_lists = []
-mean_plot_lists = []
 
 for life in lives:
-    amp_list = []
-    mean_list = []
-
-    # mean_list.append(UCS)
-    # amp_list.append(0)
-
     for i in range(len(SN_models)):
-        #----Add STC and STT points
-
         amp = 10**(float(SN_models[i].predict(np.array(life).reshape(-1, 1))))
         mean = CLD_definition.convert_to_mean_stress(amp,R_values[i])
-        amp_list.append(amp)
-        mean_list.append(mean)
-    
-    # mean_list.append(UTS)
-    # amp_list.append(0)
+        coords.append([mean, amp])
+        values.append(life)
 
-    amp_plot_lists.append(amp_list)
-    mean_plot_lists.append(mean_list)
-
-coords = np.array([])
-values = np.array([])
-# coords.append([UCS, 0], [UTS,0])
-# values.append([0],[0])
+coords = np.array(coords)
+values = np.array(values)
 print(coords, values)
 
-coords = np.array(mean_plot_lists,amp_plot_lists)
+surface = sp.interpolate.RBFInterpolator(coords, values)
+
+X = np.arange(-800, 800, 10)
+Y = np.arange(0, 800, 10)
+gridList= np.vstack(list(map(np.ravel, np.meshgrid(X, Y)))).T
+Z = surface(gridList)
+X, Y = np.meshgrid(X, Y)
+print(X,Y,Z)
+
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(X, Y, Z)
+
+plt.show()
+
