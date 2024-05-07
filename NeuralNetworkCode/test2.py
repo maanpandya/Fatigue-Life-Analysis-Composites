908e3b2ba09e0bc4bf4627013329c30a72aa79ec
maanpandya <maanbhavinpandya@gmail.com>
2024-05-07 12:02:38 +0200

    sn curve fixes

--- NeuralNetworkCode/test2.py
+++ NeuralNetworkCode/test2.py
@@ -9,7 +9,7 @@ import time
 import random as rd
 
 complete = True
-path = 'NNModelArchive/rev3/RIPtupac'
+path = 'NeuralNetworkCode/NNModelArchive/rev3/bequalzero'
 model, scaler = f.import_model(path)
 x_test = dp.dfread(path + '/x_test.csv')
 y_test = dp.dfread(path + '/y_test.csv')
